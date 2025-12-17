29fa5a83379e7c57eebd239b57a31d76971eaf1f
jsensenig <jonsensenig17@gmail.com>
2025-12-17 16:07:49 -0500

    Change status code for orchestrator

--- daemon/daq_orchestrator.cpp
+++ daemon/daq_orchestrator.cpp
@@ -325,7 +325,7 @@ void SendStatus(std::unique_ptr<TCPConnection> &status_client_ptr, quill::Logger
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         GetComputerStatus(logger); // returns vector of 0's on failure
         auto status = g_daq_monitor.serialize();
-        status_client_ptr->WriteSendBuffer(0x20, status);
+        status_client_ptr->WriteSendBuffer(to_u16(CommunicationCodes::ORC_Hardware_Status), status);
     }
 }
 
